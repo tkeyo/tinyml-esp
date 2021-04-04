168a9cf334f42e18b47e2697fffafa009f22daed
tk <kepes.tomas@gmail.com>
2021-04-04 21:21:57 +0200

    add requests and dummy data

--- tinyml/boot.py
+++ tinyml/boot.py
@@ -8,6 +8,21 @@ def do_connect():
         wlan.connect(wifi.SSID, wifi.PASS)
         while not wlan.isconnected():
             pass
-    print('Network config:', wlan.ifconfig())
+    print('Network config: ', wlan.ifconfig())
+
+def set_time():
+    import ntptime
+    import utime
+    ntptime.host = '0.europe.pool.ntp.org'
+    ntptime.settime()
+    print("Time after synchronization：%s" %str(utime.gmtime()))
+
+def set_frequency():
+    import machine
+    print('Machine freq: {} MHz'.format(int(machine.freq()/1_000_000)))
+    machine.freq(240000000)
+    print('Machine freq set to: {} MHz'.format(int(machine.freq()/1_000_000)))
 
 do_connect()
+set_time()
+set_frequency()
--- tinyml/main.py
+++ tinyml/main.py
@@ -1,34 +1,36 @@
-import _thread
 import utime
+import ujson
 import machine
 import urequests
 from machine import Timer
 
+from dummy_data import ACC_X, ACC_Y, ACC_Z
+
 print('Starting ESP32 script.')
-print('Machine Freq: ', machine.freq())
+unix_base = 946681200 # utime.gmtime(0) - epoch 0 - (2000, 1, 1, 0, 0, 0, 5, 1)
 
 
 # TODO replace this with real data
 headers = {'Content-Type': 'application/json'}
-payload_move = ujson.dumps({'time':800, 'move':1})
-payload_rms = ujson.dumps({'time':800, 'acc_x_rms':1.23, 'acc_y_rms': 3.313, 'acc_z_rms':6.21111})
 
 def post_request_move():
+    time = unix_base + utime.time()
+
+    payload_move = ujson.dumps({'time':time, 'move':1})
     res = urequests.post(
         'http://10.0.0.5:8081/api/write-move',
         data=payload_move,
         headers=headers)
-    print(response.json())
-    response.close()
+    print(res.json())
+    res.close()
 
 def post_request_rms():
+    time = unix_base + utime.time()
+
+    payload_rms = ujson.dumps({'time':time, 'acc_x_rms':1.23, 'acc_y_rms': 3.313, 'acc_z_rms':6.21111})
     res = urequests.post(
         'http://10.0.0.5:8081/api/write-rms',
         data=payload_rms,
         headers=headers)
     print(response.json())
     response.close()
-    
-while True:
-    post_request()
-    utime.sleep(5)
