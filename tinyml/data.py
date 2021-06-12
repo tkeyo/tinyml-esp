206582f45717ce50fdf6cced6612212c7d14afb9
tk <kepes.tomas@gmail.com>
2021-06-12 16:24:29 +0200

    add type hinting

--- tinyml/data.py
+++ tinyml/data.py
@@ -1,19 +1,26 @@
 import math
-# %%
 
 class Data:
     def __init__(self, freq, n_signals):
         self.n_signals = n_signals
         self.capacity = (freq * self.n_signals) + self.n_signals
         self.buffer = []
+        
+    @property
+    def size(self) -> int:
+        return len(self.buffer)
     
-    def _scale(self, vals, xmin, xmax, resolution):
+    @property
+    def data(self) -> list:
+        return self.buffer
+
+    def _scale(self, vals: list, xmin: int, xmax: int, resolution:) -> list:
         return [int((x-xmin)/(xmax-xmin)*resolution) for x in vals]
 
-    def _round(self, vals, places=4):
+    def _round(self, vals: list, places: int=4) -> list:
         return [round(x, places) for x in vals]
 
-    def _is_full(self):
+    def _is_full(self) -> bool:
         return len(self.buffer) > self.capacity
 
     def _calc_rms(self, vals: list) -> float:
@@ -21,7 +28,7 @@ class Data:
         l = len(vals)
         return math.sqrt(sum([pow(x,2) for x in vals]) / l)
  
-    def collect(self, acc=None, gyro=None):
+    def collect(self, acc: list=None, gyro:list=None) -> None:
         while self._is_full():
             self.buffer.pop(0)
         if acc:
@@ -29,15 +36,6 @@ class Data:
         if gyro:
             self.buffer.extend(gyro)
 
-    @property
-    def data(self):
-        return self.buffer
-    
-
-    def get_rms(self, signal: int):
+    def get_rms(self, signal: int) -> float:
         """Params: signal index"""
         return self._calc_rms(self.buffer[signal:][0::3])
-
-    @property
-    def size(self):
-        return len(self.buffer)
