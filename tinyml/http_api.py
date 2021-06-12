f4e9873ff5ed14aa787da854cb8792ccd957dbef
tk <kepes.tomas@gmail.com>
2021-06-12 21:45:38 +0200

    cleanup http_api

--- tinyml/http_api.py
+++ tinyml/http_api.py
@@ -17,17 +17,15 @@ def post_request_move(time, move):
 
     try:
         res = urequests.post('{}/api/write-move'.format(URL), data=payload, headers=headers)
-        print(res.content)
         res.close()
     except OSError:
         print('Server unreachable')
 
-def post_request_rms(time,x,y,z):
+def post_request_rms(time, x, y, z):
     payload = ujson.dumps({'device_id':1, 'time':time, 'acc_x_rms':x, 'acc_y_rms':y, 'acc_z_rms':z})
 
     try:
         res = urequests.post('{}/api/write-rms'.format(URL), data=payload, headers=headers)
-        print(res.content)
         res.close()
     except OSError as e:
         print('Error: {}'.format(e))
