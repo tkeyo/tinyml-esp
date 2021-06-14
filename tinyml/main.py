1228bec7d5a9c3e09b61a59273a6e37f80fc9af3
tk <kepes.tomas@gmail.com>
2021-06-14 18:13:07 +0200

    refactor main for config file + parametrized cleanup

--- tinyml/main.py
+++ tinyml/main.py
@@ -10,6 +10,7 @@ from machine import Timer, Pin, SoftI2C
 from mpu6500 import MPU6500, SF_DEG_S, SF_M_S2
 
 gc.collect()
+from config import config
 from data import Data
 from util import (get_time, get_time_diff,
                   get_final_inf_res, reduce_infs, 
@@ -20,14 +21,20 @@ from model import random_forest_a8c9ff5 as rf
 
 print('[Main] Starting ESP32 script.')
 
+
+# initialize IMU connection
 gc.collect()
 i2c = SoftI2C(scl=Pin(22), sda=Pin(21))
 mpu6500 = MPU6500(i2c, accel_sf=SF_M_S2, gyro_sf=SF_DEG_S)
 
+
+# Initialize data store
 gc.collect()
 data = Data(freq=50, n_signals=5)
 data_cap = data.capacity
 print('[Main] Data store initiated. Capacity: {}'.format(data_cap))
+
+
 
 # initialize inference collection tuples
 inf_tuples = []
@@ -63,7 +70,8 @@ def score(timer):
             inf_tuples.append((now, res))
     
     time_diff = get_time_diff(inf_tuples)
-    result = debounce(inf_tuples, time_diff, 9, 450)
+    result = debounce(inf_tuples, time_diff, 
+                      config.MIN_INF_TUPLES, config.TIME_DIFF)
     
     gc.collect()
     if result:
@@ -76,7 +84,8 @@ def score(timer):
                 }})
         inf_tuples = [] # cleans inference tuple buffer after inference
     
-    inf_tuples = clean_inf_tuples(inf_tuples, time_diff)
+    inf_tuples = clean_inf_tuples(inf_tuples, time_diff,
+                                  config.CLEAN_MAX_TUPLES, config.CLEAN_MAX_TIME_DIFF)
         
 
 def rms(timer):
@@ -99,36 +108,47 @@ def rms(timer):
 def send_data(timer):
     '''Sends queued data to API endpoint.'''
     gc.collect()
-    send_counter = 1
+    message_counter = 1
     global send_queue
     
     while send_queue:
         send_start = utime.ticks_ms()
         data_to_send = send_queue.popleft()
         request_post(data_to_send['type'], data_to_send['payload'])
-        print('Payload {}: {}'.format(send_counter, data_to_send['payload']))
-        print('Data send time: {}'.format(utime.ticks_diff(utime.ticks_ms(), send_start)))
-        send_counter += 1
+        
+        print('Payload {}: {}'.format(
+            message_counter, data_to_send['payload']))
+        print('Data send time: {}'.format(
+            utime.ticks_diff(utime.ticks_ms(), send_start)))
+        message_counter += 1
 
 
 def read_sensor():
     '''Timer to periodically read sensor values.'''
-    Timer(0).init(freq=50, mode=Timer.PERIODIC, callback=read)
+    Timer(0).init(freq=config.READ_SENSOR_FREQ,
+                  mode=Timer.PERIODIC, 
+                  callback=read)
 
 
 def run_score():
     '''Timer to periodically run scoring on collected data.'''
-    Timer(1).init(freq=50, mode=Timer.PERIODIC, callback=score)
+    Timer(1).init(freq=config.RUN_SCORE_FREQ, 
+                  mode=Timer.PERIODIC, 
+                  callback=score)
 
 
 def run_rms():
     '''Timer to periodically run RMS calculation on collected data.'''
-    Timer(2).init(period=10_000, mode=Timer.PERIODIC, callback=rms)
+    Timer(2).init(period=config.RUN_RMS_CALC_MS,
+                  mode=Timer.PERIODIC,
+                  callback=rms)
 
     
 def run_send_data():
     '''Timer to periodically send data to API endpoint.'''
-    Timer(3).init(period=5_000, mode=Timer.PERIODIC, callback=send_data)
+    Timer(3).init(period=config.RUN_SEND_DATA_MS,
+                  mode=Timer.PERIODIC,
+                  callback=send_data)
 
 
 if __name__ == '__main__':
